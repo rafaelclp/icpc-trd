d3c8d19fe38cd116f9fb26a9cf509f9cc3c0502d
unknown <rafael.clp@hotmail.com>
2015-05-13 11:30:49 -0300

    Details...

--- src/al_geometry.cpp
+++ src/al_geometry.cpp
@@ -156,7 +156,7 @@ vector<PT> CircleCircleIntersection(PT a, PT b, double r, double R) {
 // the "center of gravity" or "center of mass".
 double ComputeSignedArea(const vector<PT> &p) {
 	double area = 0;
-	for(int i = 0; i < p.size(); i++) {
+	for (int i = 0; i < p.size(); i++) {
 		int j = (i+1) % p.size();
 		area += p[i].x*p[j].y - p[j].x*p[i].y;
 	}
