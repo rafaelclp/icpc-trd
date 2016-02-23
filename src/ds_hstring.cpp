248371140815d9ea5a92d5c8d3b4972cd0746944
unknown <rafael.clp@hotmail.com>
2016-02-23 07:08:08 -0300

    Minor fix - B^[-i] to B[-i]

--- src/ds_hstring.cpp
+++ src/ds_hstring.cpp
@@ -1,6 +1,6 @@
 Gerando bases B:
 	B[i] = BASE^i % m, 0 <= i
-	B^[-i] = BASE^(m-1-i) % m, 1 <= i
+	B[-i] = BASE^(m-1-i) % m, 1 <= i
 Gerando hash H para uma string S de tamanho n+1:
 	H = (S[0] + S[1]*B[1] + ... + S[n]*B[n]) % m
 	H[n] = (H[n-1] + S[n]*B[n]) % m
