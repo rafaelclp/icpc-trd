7cd562a48d69cdc535786b88669bd77f70b9efcf
unknown <rafael.clp@hotmail.com>
2016-06-16 21:20:00 -0300

    Starting the new trd

---
+++ new-src/README.md
@@ -0,0 +1,3 @@
+## UFCG Team Referece Document
+
+I'm rebuilding the TRD [almost] from scratch with my own fully tested implementations. It will take time, since I have over a year and a half to do it.
---
+++ new-src/ds_segtree.cpp
@@ -0,0 +1,23 @@
+template<class T> struct segtree {
+	int n; T *t; // t[1] is root, t[0] is not used
+	inline T op(T &lval, T &rval) { return max(lval, rval); } // CHANGE THIS!!
+	inline void build(vector<T> &v) { // you may want to remove call to alloc!
+		alloc(v.size()); for (int i = 0; i < n; i++) t[i+n] = v[i];
+		for (int i = n - 1; i > 0; i--) t[i] = op(t[i<<1], t[i<<1|1]);
+	}
+	inline void modify(int p, const T &val) {
+		for (t[p += n] = val; p >>= 1; ) t[p] = op(t[p<<1], t[p<<1|1]);
+	}
+	inline T query(int l, int r) { // [l, r); for single element, get t[n+idx]
+		T res = T(); // nil (such that op(v, nil) == v)
+		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+			if (l&1) res = op(res, t[l++]); // if order really matters, create
+			if (r&1) res = op(t[--r], res); // resl and resr and merge in retn
+		}
+		return res; // op(resl, resr)
+	}
+	static T pool[]; static int poolIdx; static void free() { poolIdx = 0; }
+	inline void alloc(int size) { t = pool+poolIdx; poolIdx += (n=size)<<1; }
+}; // (1) segtree<int> seg; (2) seg.build(v); OR seg.alloc(n); (doesn't clear)
+template<class T> T segtree<T>::pool[2*MAXN]; // (3) after EVERY testcase, use
+template<class T> int segtree<T>::poolIdx;    // segtree<int>::free();
